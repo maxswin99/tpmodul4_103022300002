2517db3ba65a1ac0d4738a64cb8a8e54f97129b9
maxswin99 <andreaschristianfirga06@gmail.com>
2025-03-10 03:44:20 +0700

    Update Program.cs

--- tpmodul4_103022300002/Program.cs
+++ tpmodul4_103022300002/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        KodePos kodePos = new KodePos();
+        Console.WriteLine("Kode Pos Batununggal: " + kodePos.GetKodePos("Batununggal"));
+        Console.WriteLine("Kode Pos Cijaura: " + kodePos.GetKodePos("Cijaura"));
+
+        
+        DoorMachine door = new DoorMachine();
+        door.Unlock();
+        door.Lock();
+    }
+}
